9435659627b5591234d97e582e51d36f3a525ff8
Sebastian Ehlert <28669218+awvwgk@users.noreply.github.com>
2022-06-30 17:40:44 +0200

    Update to version 5.0.0
    MNT: Re-rendered with conda-build 3.21.9, conda-smithy 3.21.0, and conda-forge-pinning 2022.06.30.11.45.20

---
+++ recipe/build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+set -ex
+
+cmake \
+    ${CMAKE_ARGS} \
+    -B _build -G Ninja \
+    -DSCINE_MARCH=""
+
+cmake --build _build
+if [[ "${CONDA_BUILD_CROSS_COMPILATION:-0}" == "0" ]]; then
+  ctest --test-dir _build --output-on-failure
+fi
+cmake --install _build
